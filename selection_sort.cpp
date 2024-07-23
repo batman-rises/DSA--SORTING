e6371dcc5cf5758132427b38ae7c4eea7ee99028
Binayak Panda <pbinayak604@gmail.com>
2024-07-23 09:40:38 +0530

    updateddd

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,46 +1,19 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-// Function to perform selection sort
-void selectionSort(vector<int> &arr)
+void selection_sort(vector<int> arr)
 {
     int n = arr.size();
-    for (int i = 0; i < n - 1; ++i)
+
+    for (int i = 0; i <= n - 2; i++)
     {
-        // Find the minimum element in the unsorted part
-        int minIndex = i;
-        for (int j = i + 1; j < n; ++j)
+        int min = i;
+        for (int j = i; j < n; j++)
         {
-            if (arr[j] < arr[minIndex])
+            if (arr[j] < arr[min])
             {
-                minIndex = j;
+                min = j;
             }
+            swap(arr[i], arr[min]);
         }
-        // Swap the found minimum element with the first element
-        swap(arr[i], arr[minIndex]);
     }
-}
-
-// Function to print the array
-void printArray(const vector<int> &arr)
-{
-    for (int elem : arr)
-    {
-        cout << elem << " ";
-    }
-    cout << endl;
-}
-
-int main()
-{
-    vector<int> arr = {64, 25, 12, 22, 11};
-    cout << "Original array: ";
-    printArray(arr);
-
-    selectionSort(arr);
-
-    cout << "Sorted array: ";
-    printArray(arr);
-
-    return 0;
 }
